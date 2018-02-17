27c04a76d640ed44e1cc8074116f5b6daf970064
Kevin Pansawira <kpansawi@ucsd.edu>
2018-02-17 15:27:46 -0800

    Implemented login

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-	function addCompany(name, id, bannerUrl, hq, size, industry, website, photosUrl, logoUrl, title, description) {
+	function addCompany(username,password,name, id, bannerUrl, hq, size, industry, website, photosUrl, logoUrl, title, description) {
 	    var newComp = Object.create(Company);
 
 	    newComp.name = name;
@@ -13,6 +13,8 @@
 	    newComp.openPositions = [];
 	    newComp.title = title;
 	    newComp.description = description
+	    newComp.username=username;
+	    newComp.password=password;
 	    console.log(newComp);
 	    this.allCompanies.push(newComp);
 	}
@@ -44,9 +46,9 @@
 	}
 
 	function populateCompanies() {
-	    this.addCompany("Amazon, Inc.", "1", "amazon.PNG", "Seattle, WA", "75000", ["E-Commerce", "Cloud Computing"], "amazon.com", ["amazon1.JPG", "amazon2.JPG", "amazon3.JPG", "amazon4.JPG", "amazon5.JPG", "amazon6.JPG"], "amazon.jpg",
+	    this.addCompany("recruiting@amazon.com","amazon12345","Amazon, Inc.", "1", "amazon.PNG", "Seattle, WA", "75000", ["E-Commerce", "Cloud Computing"], "amazon.com", ["amazon1.JPG", "amazon2.JPG", "amazon3.JPG", "amazon4.JPG", "amazon5.JPG", "amazon6.JPG"], "amazon.jpg",
 	        "We pioneer", "We're a company of pioneers. It's our job to make bold bets, and we get our energy from inventing on behalf of customers. Success is measured against the possible, not the probable. For today’s pioneers, that’s exactly why there’s no place on Earth they’d rather build than Amazon.");
-	    this.addCompany("Salesforce", "2", "Salesforce.PNG", "San Francisco, WA", "10000", ["Cloud Computing"],
+	    this.addCompany("recruiting@salesforce.com","salesforce12345","Salesforce", "2", "Salesforce.PNG", "San Francisco, WA", "10000", ["Cloud Computing"],
 	        "Salesforce.com", ["salesforce1.JPG", "salesforce2.JPG", "salesforce3.JPG"], "salesforce.jpg",
 	        "We pioneer", "We're a company of pioneers. It's our job to make bold bets, and we get our energy from inventing on behalf of customers. Success is measured against the possible, not the probable. For today’s pioneers, that’s exactly why there’s no place on Earth they’d rather build than Amazon.");
 
@@ -73,7 +75,7 @@
 	}
 
 	//-------------STUDENT OBJECT
-	function addStudent(name, sid, school, year, major, GPA, minor, Pic) {
+	function addStudent(name, sid, school, year, major, GPA, minor, Pic,username,password) {
 	    var newStudent = Object.create(Student);
 
 	    newStudent.name = name;
@@ -86,50 +88,55 @@
 	    newStudent.profilePic = Pic;
 	    newStudent.status = "Looking for internship";
 	    newStudent.applications = [];
+	    newStudent.username = username;
+	    newStudent.password = password;
 
 	    this.allStudents.push(newStudent);
 	}
 
 	function populateStudents() {
-	    this.addStudent("Kevin Pansawira", "1", "UCSD", "2018", "Computer Science", "3.99", "none", "kpan.jpg");
-	    this.addStudent("Michael Angelo", "2", "MIT", "2018", "Arts", "3.98", "none", "kpan.jpg");
+	    this.addStudent("Kevin Pansawira", "1", "UCSD", "2018", "Computer Science", "3.99", "none", "kpan.jpg","kp","kp12345");
+	    this.addStudent("Michael Angelo", "2", "MIT", "2018", "Arts", "3.98", "none", "kpan.jpg","ma","ma12345");
 	    console.log("populateStudents done");
 	}
 
 function onClickLogin(){
-	var type = document.getElementById("type").value;
+	console.log(document.getElementById("userType"));
+	var type = document.getElementById("userType").value;
 	var uname = document.getElementById("uname").value;
 	var psw = document.getElementById("psw").value;
 
 	if(type == "c"){
 		for(var i = 0; i < allCompanies.length; i++){
-			if(allCompanies[i].userName == uname && allCompanies[i].password == psw){
+			console.log("i="+ i.toString());
+			if(allCompanies[i].username == uname && allCompanies[i].password == psw){
 				localStorage.setItem("userType",type);
 				localStorage.setItem("userId",allCompanies[i].id);
 				alert("Sign in Successful!");
-				window.location = "company.html";	
+				location.href = "company.html?companyId=" + allCompanies[i].id;
+				break;	
 			}
 		}
 	}
 
-	if(type == "s"){
+	else if(type == "s"){
 		for(var i = 0; i < allCompanies.length; i++){
-			if(allCompanies[i].userName == uname && allCompanies[i].password == psw){
+			if(allStudents[i].username == uname && allStudents[i].password == psw){
 				localStorage.setItem("userType",type);
-				localStorage.setItem("userId",allCompanies[i].id);
+				localStorage.setItem("userId",allStudents[i].id);
 				alert("Sign in Successful!");
-				window.location = "profile.html";	
+				location.href = "profile_page.html?studentId=" + allStudents[i].sid;	
 			}
 		}
 	}
 
-	if(type == "t"){
+	else if(type == "t"){
 		for(var i = 0; i < allCompanies.length; i++){
-			if(allCompanies[i].userName == uname && allCompanies[i].password == psw){
+			if(allStudents[i].userName == uname && allCompanies[i].password == psw){
 				localStorage.setItem("userType",type);
 				localStorage.setItem("userId",allCompanies[i].id);
 				alert("Sign in Successful!");
-				window.location = "profile.html";	
+				location.href = "profile.html";	
 			}
 		}
 	}
