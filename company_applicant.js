97c1a40d9090a9499487077205efe5579ace27e3
Junnel Reboquio <jreboqui@ucsd.edu>
2018-02-18 08:58:33 -0800

    company applicant

--- company_applicant.js
+++ company_applicant.js
@@ -51,6 +51,66 @@ function populateStudentProfile(studentId) {
     info.appendChild(status);
 }
 
+function setButtons(){
+    console.log("setButtons called");
+    if(userType != 'c'){
+        document.getElementById("offer").style.display='none';
+        document.getElementById("reject").style.display='none';
+    }
+
+}
+
+function populateOtherApplicants(companyId, jobId, studentId){
+	var list = document.getElementById('ul-applicants');
+	console.log("in populateOtherApplicants");
+
+	for(var i = 0; i < allStudents.length; i++){
+		console.log("i");
+		for(var j = 0; j < allStudents[i].applications.length; j++){
+			if(companyId == allStudents[i].applications[j].companyId && 
+				allStudents[i].applications[j].positionId == jobId){
+					console.log("inside");
+					var item = document.createElement('li');
+					var photo = document.createElement('img');
+					photo.setAttribute('src',allStudents[i].profilePic);
+					photo.setAttribute('alt','image');
+					photo.setAttribute('height','200px');
+					photo.setAttribute('width','200px');
+					//photo.setAttribute('href',"company_applicant.html?studentId=" +allStudents[i].sid+
+						//"&companyId" + companyId + "&jobId=" + jobId);
+					item.appendChild(photo);
+
+					var detail = document.createElement('h4curr');
+
+					var name = document.createTextNode(allStudents[i].name);
+					var br = document.createElement("br");
+					detail.appendChild(name);
+					detail.appendChild(br);
+
+					var school = document.createTextNode(allStudents[i].school);
+					var br = document.createElement("br");
+					detail.appendChild(school);
+					detail.appendChild(br);
+
+					var gpa = document.createTextNode("GPA: " + allStudents[i].GPA);
+					var br = document.createElement("br");
+					detail.appendChild(gpa);
+					detail.appendChild(br);
+
+					var a = document.createElement("a");
+					a.textContent = allStudents[i].applications[j].appStatus;
+					a.setAttribute('href',"company_applicant.html?studentId=" +allStudents[i].sid+
+						"&companyId" + companyId + "&jobId=" + jobId);
+					detail.appendChild(a);
+
+					item.appendChild(detail);
+					list.appendChild(item);
+					break;
+			}
+		}
+	}
+}
+
 
 function retrieveLocalData1(){
     var retrivedCompanies = localStorage.getItem('allCompanies');
@@ -60,6 +120,8 @@ function retrieveLocalData1(){
     var retrievedStudents = localStorage.getItem('allStudents');
     retrievedStudents = JSON.parse(retrievedStudents);
     allStudents = retrievedStudents;
+
+    userType = localStorage.getItem('userType');
 }
 
 function getParameterByName(name, url) {
@@ -78,4 +140,6 @@ window.onload = function() {
     companyId = getParameterByName('companyId');
     jobId = getParameterByName('jobId');
     populateStudentProfile(studentId);
+    setButtons();
+    populateOtherApplicants(companyId, jobId, studentId);
 }
